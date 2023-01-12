a79e5740ef6c0bbe553140c0387cb601b17f9245
Leonardo Piekala <37380454+Piekala@users.noreply.github.com>
2023-01-12 11:50:35 -0300

    Create haskell03.hs

---
+++ haskell03.hs
@@ -0,0 +1,58 @@
+--Exercicios realizados em aula:
+
+--01:
+add10toall :: [Int] -> [Int]
+add10toall x = [ x+10 | x <- x ]
+
+--02:
+multN :: Int -> [Int] -> [Int]
+multN x list = [ list * x | list <- list ]
+
+--03:
+multN' :: Int -> [Int] -> [Int]
+multN' x list = map (*x) list
+
+--04:
+applyExpr :: [Int] -> [Int]
+applyExpr x = [ 3*x+2 | x <- x ]
+
+--05:
+applyExpr' :: [Int] -> [Int]
+applyExpr' x = map ( \y -> 3*y+2 ) x
+
+--06:
+addSuffix :: String -> [String] -> [String]
+addSuffix string list = [ list++string | list <- list ]
+
+--07:
+selectgt5 :: [Int] -> [Int]
+selectgt5 x = [x | x <- x, x > 5 ]
+
+--08:
+sumOdds :: [Int] -> Int
+sumOdds x = sum (filter (\y -> odd y )x)
+
+--09: 
+sumOdds' :: [Int] -> Int
+sumOdds' x = sum[ x | x <- x, odd x]
+
+--10:
+selectExpr :: [Int] -> [Int]
+selectExpr x = [ x | x <- x, x >= 20, x <= 50 ]
+
+--11:
+countShorts :: [String] -> Int
+countShorts x = length ([x | x <- x, length x < 5])
+
+--12:
+calc12 :: Float -> Float
+calc12 x = (x^2) /2
+
+calcExpr :: [Float] -> [Float]
+calcExpr x = [ calc12 x | x <- x, calc12 x > 10]
+
+--13:
+
+
+
+--Exercicios realizados em casa:
